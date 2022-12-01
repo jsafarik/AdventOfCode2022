9ba0361c5a7a3668f1bb7593d02c397bab031d34
Jan Safarik <jsafarik@redhat.com>
2022-12-01 12:05:31 +0100

    [Day 1] Puzzle 2

--- src/main.rs
+++ src/main.rs
@@ -2,10 +2,22 @@ fn main() {
     let inventory_str = include_str!("resources/day_1_elves_inventory.txt");
     let (index, calories) = get_elf_with_most_food_in_calories(inventory_str);
     println!("Elf #{} has the most calories ({})", index, calories);
+
+    let top_3_elves = get_3_elves_with_most_food_in_calories(inventory_str);
+    let mut total = 0;
+    for (index, calories) in &top_3_elves {
+        println!("Elf #{} belongs to top 3 with {}", index, calories);
+        total += calories
+    }
+    println!("Top 3 elves have {} calories in total", total);
 }
 
 fn get_elf_with_most_food_in_calories(inventory_str: &str) -> (usize, u32) {
-    inventory_str
+    get_3_elves_with_most_food_in_calories(inventory_str)[0]
+}
+
+fn get_3_elves_with_most_food_in_calories(inventory_str: &str) -> Vec<(usize, u32)> {
+    let mut totals: Vec<(usize, u32)> = inventory_str
         .split("\n\n")
         .map(|inv: &str| inv
             .lines()
@@ -13,13 +25,15 @@ fn get_elf_with_most_food_in_calories(inventory_str: &str) -> (usize, u32) {
             .fold(0, |acc: u32, x: u32| acc + x)
         )
         .enumerate()
-        .max_by(|(_, a), (_, b)| a.cmp(b))
-        .expect("Couldn't find max value")
+        .collect();
+    totals.sort_by(|(_, a), (_, b)| b.cmp(a));
+    totals.truncate(3);
+    totals
 }
 
 #[cfg(test)]
 mod tests {
-    use crate::get_elf_with_most_food_in_calories;
+    use crate::{get_3_elves_with_most_food_in_calories, get_elf_with_most_food_in_calories};
 
     #[test]
     fn example_inventory_max_calories() {
@@ -34,6 +48,16 @@ mod tests {
             }
             assert_ne!(index, idx);
             assert_ne!(calories, result.clone());
+        }
+    }
+
+    #[test]
+    fn example_inventory_top_3_calories() {
+        let inventory_str = include_str!("resources/test/day_1_elves_inventory_example.txt");
+        let top_3_elves = get_3_elves_with_most_food_in_calories(inventory_str);
+        let results: [u32; 3] = [24000, 11000, 10000];
+        for (idx, result) in results.iter().enumerate() {
+            assert_eq!(top_3_elves[idx].1, result.clone());
         }
     }
 }
